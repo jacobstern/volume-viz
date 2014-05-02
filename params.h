432c0608e9ed92a38c4867b9dc8e0532b7caae96
Robin Martens <robin_martens@brown.edu>
2014-05-02 15:09:21 -0400

    ready to present

--- params.h
+++ params.h
@@ -37,11 +37,11 @@ static const char *g_texture_paths[N_DEFAULT_TEXTURES] = {"/home/rmartens/shared
 
 static char *g_savepath_default = "Save slice as:";
 
-//static char* g_slice_slider_captions[N_SLICE_SLIDERS] = {"x offset", "y offset", "z offset",
-//                                                         "x rotation", "y rotation", "z rotation"};
+static char* g_slice_slider_captions[N_SLICE_SLIDERS] = {"x offset", "y offset", "z offset",
+                                                         "x rotation", "y rotation", "z rotation"};
 
-static char* g_slice_slider_captions[N_SLICE_SLIDERS] = {"dx", "dy", "dz",
-                                                         "theta", "phi", "psi"};
+//static char* g_slice_slider_captions[N_SLICE_SLIDERS] = {"dx", "dy", "dz",
+//                                                         "theta", "phi", "psi"};
 
 typedef enum{ HORIZONTAL, SAGITTAL, CORONAL, N_CANONICAL_ORIENTATIONS, FREE_FORM} canonicalOrientation;
 static char* g_canonical_orientation_captions[N_CANONICAL_ORIENTATIONS] = {"horizontal", "sagittal", "coronal"};
